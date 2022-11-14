d118afee9400fb680d052d4e13b02aef7fd31487
vinkayasmin <118167440+vinkayasmin@users.noreply.github.com>
2022-11-14 15:52:09 +0700

    Add files via upload

---
+++ Grade.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+int main() {
+
+int nilai;
+
+printf("Input Nilai\t :");
+
+scanf("%i", &nilai);
+
+if(nilai >= 80 && nilai <=100)
+
+{
+
+printf("Grade nilai\t: Sangat Baik");
+
+}
+
+else if(nilai >= 70 && nilai <=79)
+
+{
+
+printf("Grade nilai\t: Baik");
+
+}
+
+else if(nilai >= 60 && nilai <=69)
+
+{
+
+printf(“Grade nilai\t: Cukup”);
+
+}
+
+else if(nilai >= 50 && nilai <=59)
+
+{
+
+printf("Grade nilai\t: Kurang");
+
+}
+
+else
+
+{
+
+printf("Grade nilai\t: Gagal");
+
+}
+
+return 0;
+}
